1c65ea639cdfd3463e546fb4124982a49b43478f
Dafydd Crosby <dtcrsby@gmail.com>
2014-04-22 22:16:46 -0600

    Initial commit of Calgary Hub rebuild

---
+++ Gemfile
@@ -0,0 +1,15 @@
+# If you have OpenSSL installed, we recommend updating
+# the following line to use "https"
+source 'https://rubygems.org'
+
+gem "middleman", "~>3.3.2"
+
+gem "therubyracer"
+
+# Live-reloading plugin
+gem "middleman-livereload", "~> 3.1.0"
+
+# For faster file watcher updates on Windows:
+gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
+
+gem "multi_json", '1.7.8'
---
+++ config.rb
@@ -0,0 +1,89 @@
+###
+# Compass
+###
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+# activate :livereload
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'css'
+
+set :js_dir, 'js'
+
+set :images_dir, 'img'
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+
+  activate :directory_indexes
+end
+
+page "/about.html"
+page "/locations.html"
+page "/events.html"
+page "/activities.html"
+
+data.locations.each do |id, place|
+  proxy "/location/#{id}.html", "/location_template.html", :locals => { :place => place }, :ignore => true
+end
+
+data.events.each do |id, event|
+  proxy "/event/#{id}.html", "/event_template.html", :locals => { :event => event }, :ignore => true
+end
+
+data.activities.each do |id, activity|
+  proxy "/activity/#{id}.html", "/activity_template.html", :locals => { :activity => activity }, :ignore => true
+end
